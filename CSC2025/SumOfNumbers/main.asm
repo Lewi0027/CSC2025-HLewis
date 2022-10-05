93da7fd9c863b7c1ef561841e80d4e5e84430a52
Lewis <S02415179@ccc.ccofc.edu>
2022-10-05 16:43:19 -0600

    add

---
+++ CSC2025/SumOfNumbers/main.asm
@@ -0,0 +1,21 @@
+.386P
+.model flat
+extern _ExitProcess@4: near
+
+.data
+
+
+.code
+main PROC near
+_main:
+
+	; add all values from 0-10
+	mov		eax, 1
+	mov		ebx, 2
+	add		eax, ebx
+	
+	push 0
+	call _ExitProcess@4
+
+main ENDP
+END
