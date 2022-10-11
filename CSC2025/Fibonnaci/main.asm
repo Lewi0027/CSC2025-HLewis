1b69bed671bf721c8175d7daff1b9f73c623816e
Henry Lewis <lewi0027@yahoo.com>
2022-10-10 18:47:38 -0600

    Exchange/Fibonnaci

---
+++ CSC2025/Fibonnaci/main.asm
@@ -0,0 +1,45 @@
+.386P
+.model flat
+extern _GetStdHandle@4: near
+extern _ExitProcess@4: near
+extern _WriteConsoleA@20: near
+extern _ReadConsoleA@20: near
+
+.data
+
+previous	byte	1
+current		byte	1
+next		byte	0
+i			byte	3
+n			byte	10; adjust as needed
+
+.code
+main PROC near
+_main:
+
+	mov		ah, previous
+	mov		al, current
+	mov		bh, next
+	mov		bl, n
+
+_loop:
+	; next = current + previous
+	add		ah, al
+	mov		bh, ah
+
+	; previous=current
+	mov		ah, al
+
+	; current = next
+	mov		al, bh
+
+	inc		i
+	cmp		i, bl
+	jle		_loop
+
+
+	push	0
+	call	_ExitProcess@4
+
+main ENDP
+END
